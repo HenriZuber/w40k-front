1cc3de921d887d973193969533a784c6207194de
RonMellerio <rolandmellerio@gmail.com>
2023-06-23 20:26:55 +0200

    fix: starting app while cookie already set directs to chat instead of settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import Cookies from 'js-cookie';
-import './style/App.css';
-import ChoiceDropdown from './ChoiceDropdown';
-import ChatApp from './ChatApp';
-import Settings from './Settings'
+import React, { useState, useEffect } from "react";
+import Cookies from "js-cookie";
+import "./style/App.css";
+import ChoiceDropdown from "./ChoiceDropdown";
+import ChatApp from "./ChatApp";
+import Settings from "./Settings";
 
 function App() {
-
   const [activeComponent, setActiveComponent] = useState("ChoiceDropdown");
 
   useEffect(() => {
     // Check if the choice is already saved in a cookie
-    const savedChoice = Cookies.get('choice');
+    const savedChoice = Cookies.get("choice");
     if (savedChoice) {
       const choiceArray = JSON.parse(savedChoice);
       console.log(choiceArray);
-      setActiveComponent("Settings");
+      setActiveComponent("ChatApp");
     }
   }, []);
 
@@ -28,10 +27,16 @@ function App() {
       <header className="App-header">
         <h3>Wahabot</h3>
       </header>
-      <body className='App-body'>
-        {activeComponent === "ChoiceDropdown" && <ChoiceDropdown onArmySaved={handleActiveComponent} />}
-        {activeComponent === "ChatApp" && <ChatApp onSettingsClicked={handleActiveComponent} />}
-        {activeComponent === "Settings" && <Settings onSaveClicked={handleActiveComponent} />}
+      <body className="App-body">
+        {activeComponent === "ChoiceDropdown" && (
+          <ChoiceDropdown onArmySaved={handleActiveComponent} />
+        )}
+        {activeComponent === "ChatApp" && (
+          <ChatApp onSettingsClicked={handleActiveComponent} />
+        )}
+        {activeComponent === "Settings" && (
+          <Settings onSaveClicked={handleActiveComponent} />
+        )}
       </body>
     </>
   );
